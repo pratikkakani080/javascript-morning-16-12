caa0b68582299f4d766fa49869b7b85366fa90f2
Your Name <you@example.com>
2024-12-23 11:33:49 +0530

    array methods

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-console.log("this is external js file");
+// console.log("this is external js file");
 
 let age = 34;
 age = 56;
-console.log("age**", age);
+// console.log("age**", age);
 
 var firstName = 'john';
 var firstName = 'kalavati';
 firstName = 'test';
-console.log("firstName**", firstName);
+// console.log("firstName**", firstName);
 
 const variable3 = 45;
-console.log("variable3**", variable3);
+// console.log("variable3**", variable3);
 
 // casing
 // camelCase
@@ -23,7 +23,7 @@ console.log("variable3**", variable3);
     // Premitive
         // String
         const ageVar = 5
-            '', "", ``, String(444), console.log(`my age age age is ${ageVar}`.match(/age/g))
+            // '', "", ``, String(444), console.log(`my age age age is ${ageVar}`.match(/age/g))
         // Number
             0 - 9,  Number('343') // 2^53  NaN
         // Boolean
@@ -49,38 +49,52 @@ console.log("variable3**", variable3);
             //     firstName: 'test'    
             // }
             const newObj = Object.assign({ init: 'data' }, { data: () => {} }, { data2: 'test2'})
-            console.log('object creation******', newObj)
+            // console.log('object creation******', newObj)
             // newObj.data()
             // newObj['init']
             // Object desctructuring
             const { init, data, data2 } = newObj
-            console.log('object desctructuring******', init, data, data2)
+            // console.log('object desctructuring******', init, data, data2)
 
             // Object to array conversion
                 const keyList = Object.keys(newObj)
-                console.log('keyList**', keyList);
+                // console.log('keyList**', keyList);
 
                 const valueList = Object.values(newObj)
-                console.log('valueList**', valueList); 
+                // console.log('valueList**', valueList); 
                 
                 const keyValueList = Object.entries(newObj)
-                console.log('keyValueList**', keyValueList); 
+                // console.log('keyValueList**', keyValueList); 
             
             delete newObj.data2
             delete newObj.data
-            console.log('object creation after delete******', newObj)
+            // console.log('o bject creation after delete******', newObj)
 
         // Array
-            // [
-            //     1, 4, 7, 5, 6, 4, 8, 7, 5, 4, 3 
-            // ]
+           const arrrraayyyy = [
+                1, 4, 7, 5, 6, 4, 8, 7, 5, 4, 3 
+            ]
+            const poppedEl = arrrraayyyy.pop()
+            arrrraayyyy.push(56)
+
+            
+            const shiftedEl = arrrraayyyy.shift()
+
+
+             arrrraayyyy.unshift(555)
+
+
+            console.log('array **', arrrraayyyy, poppedEl, shiftedEl);
+
+                 
+            
         // Function
             function fun1() {
                 const test = 'test'
             }
             // Arrow function
             const test = () => {
-                console.log('test***********');
+                // console.log('test***********');
             }
             test()
             // callBack function

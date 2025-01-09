17fdb6e6e2d8b1506e43119f9ab24a7c2b722d50
Your Name <you@example.com>
2025-01-09 11:17:07 +0530

    feat: dom manipulation

--- DOMManipulation.js
+++ DOMManipulation.js
@@ -1,4 +1,18 @@
-console.log(document.getElementById('h1tag'))
-console.log(document.getElementsByClassName('temp-class'));
-console.log(document.getElementsByTagName('h1')[0]);
+// console.log(document.getElementById('h1tag'))
+// console.log(document.getElementsByClassName('temp-class'));
+// console.log(document.getElementsByTagName('h1')[0]);
+// console.log(document.querySelector('.temp-class'));
+// console.log(document.querySelectorAll('.temp-class'));
 
+const ddd = document.querySelector('.temp-class')
+ddd.style.color = 'blue'
+
+ddd.addEventListener('mouseover', () => {
+    console.log('h1 tag is clicked with event listner');
+    
+})
+
+ddd.removeEventListener('mouseover', () => {
+    console.log('event is removed from H1 el');
+
+})
